35d625a4b2d2698eb184ce22b5d30a472bf67fcd
abdul kabugu <kabuguabdul2@gmail.com>
2022-12-01 10:01:22 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useAccount, useConnect,} from 'wagmi'
+import {InjectedConnector} from "wagmi/connectors/injected"
 function App() {
+  const {address, isConnected} = useAccount()
+  const {connect} = useConnect({
+    connector : new InjectedConnector()
+   })
   return (
     <div className="App">
         <h1>this is testing  page</h1>
+        <button onClick={connect}>connect wallet</button>
+          {isConnected && <h3>{address}</h3>}
     </div>
   );
 }
