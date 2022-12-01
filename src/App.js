cb2268450d0394a0cddee12ff16a04e9fdb6466f
abdul kabugu <kabuguabdul2@gmail.com>
2022-12-01 10:08:26 +0300

    added second paragraph

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
         <h1>this is testing  page</h1>
+        <h1>The  second  paragraph</h1>
         <button onClick={connect}>connect wallet</button>
           {isConnected && <h3>{address}</h3>}
     </div>
