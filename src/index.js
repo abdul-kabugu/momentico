5fb67d4a3c2ad9449c03f128921d9eba1817451b
abdul kabugu <kabuguabdul2@gmail.com>
2022-12-01 08:51:51 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {WagmiConfig, configureChains, defaultChains, chain, createClient} from 'wagmi'
+import {publicProvider} from 'wagmi/providers/public'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const { provider, webSocketProvider } = configureChains(defaultChains, [
+  publicProvider(),
+])
+
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+})
 root.render(
   <React.StrictMode>
+    
+  <WagmiConfig client={client}>
     <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
 
